5bb403d351b8d90e46bca526dbdf836150b98d3b
TimHermens <lewonas@live.nl>
2014-04-18 08:33:19 -0700

    testing

---
+++ dera.h
@@ -0,0 +1,13 @@
+//
+//  dera.h
+//  FlyFlyIPad
+//
+//  Created by Tim on 18/04/2014.
+//  Copyright (c) 2014 edu.fontys.sm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dera : NSObject
+
+@end
---
+++ dera.m
@@ -0,0 +1,13 @@
+//
+//  dera.m
+//  FlyFlyIPad
+//
+//  Created by Tim on 18/04/2014.
+//  Copyright (c) 2014 edu.fontys.sm. All rights reserved.
+//
+
+#import "dera.h"
+
+@implementation dera
+
+@end
